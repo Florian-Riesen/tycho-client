bef046ff5a3f81edd9724cec8fe5bfa2d14eaec2
Skatterbrayne <florianriesen@yahoo.de>
2022-01-17 21:59:13 +0100

    Created demo app in VS

---
+++ TychoClient/TychoClient/TychoClient/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using TychoClient.Models;
+
+namespace TychoClient.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
