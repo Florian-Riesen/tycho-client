2e6cd0312bb04b6eb254cf802757b9f12f82cf9b
Skatterbrayne <florianriesen@yahoo.de>
2022-02-06 21:03:52 +0100

    ReadCard Transaction label now changes based on which way the transaction went

---
+++ TychoClient/TychoClient/TychoClient/Converters/IsNegativeConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace TychoClient.Converters
+{
+    public class IsNegativeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Int32.Parse(value.ToString()) < 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
